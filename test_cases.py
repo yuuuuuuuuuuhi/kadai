ab1189c376c472676ed27ce1e76b5533652dcf4b
yuhiii <18093@hakodate.kosen-ac.jp>
2024-01-28 15:01:29 +0900

    Add test cases for calc() function

--- test_cases.py
+++ test_cases.py
@@ -1,21 +1,34 @@
-#!/usr/bin/python3
-
 import unittest
 from calc_mul import calc
 
-# Run with testrunner so needs to be in file test_
+class TestCalc(unittest.TestCase):
 
-class TestCalc (unittest.TestCase):
+    def test_positive_integers(self):
+        self.assertEqual(calc(3, 7), 21)
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+    def test_string_inputs(self):
+        self.assertEqual(calc('a', 'b'), -1)
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+    def test_decimal_inputs(self):
+        self.assertEqual(calc(0.1, 999), -1)
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+    def test_out_of_range_integers(self):
+        self.assertEqual(calc(0, 150), -1)
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+    def test_zero_multiplication(self):
+        self.assertEqual(calc(0, 1), -1)
 
+    def test_max_range_multiplication(self):
+        self.assertEqual(calc(998, 999), 997002)
+
+    def test_near_max_range_multiplication(self):
+        self.assertEqual(calc(999, 999), -1)
+
+    def test_negative_and_positive_input(self):
+        self.assertEqual(calc(-1, 1), -1)
+
+    def test_large_positive_input(self):
+        self.assertEqual(calc(1, 1000), -1)
+
+if __name__ == '__main__':
+    unittest.main()
